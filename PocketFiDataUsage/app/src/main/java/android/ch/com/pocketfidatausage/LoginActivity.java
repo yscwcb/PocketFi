3671552e371947a1f922ff43ec505494034e3fdd
yscwcb <yscwcb@gmail.com>
2015-10-31 09:51:06 +0900

    2s sleep for network communication

--- PocketFiDataUsage/app/src/main/java/android/ch/com/pocketfidatausage/LoginActivity.java
+++ PocketFiDataUsage/app/src/main/java/android/ch/com/pocketfidatausage/LoginActivity.java
@@ -110,6 +110,7 @@ public class LoginActivity extends AppCompatActivity {
         nt.start();
 
         try {
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
